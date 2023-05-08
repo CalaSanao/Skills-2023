592bdf6bc9ed3c5d95be398377f60a05280b62cd
CalaSanao <adriancolom@paucasesnovescifp.cat>
2023-05-08 09:07:27 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,27 @@
+import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import 'bootstrap'
+import '../css/main.scss'
+import Layout from './Layout/main.vue'
+
+createInertiaApp({
+    resolve: async name => {
+        const pages = import.meta.glob('./Pages/**/*.vue')
+        let page = await pages[`./Pages/${name}.vue`]()
+
+        if (page.default.layout === undefined) {
+            page.default.layout = Layout
+        }
+        return page
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+    progress: {
+        color: 'blue',
+        showSpinner: true,
+    }
+})
